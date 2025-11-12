45d7fc9953ee792906bd1a85a90b84e01d6a0dfb
Abdul Rehman <k250790@nu.edu.pk>
2025-11-12 22:13:16 +0500

    Refactor taxCalc to return net income via pointer

--- lab-09-tasksPF/6.cpp
+++ lab-09-tasksPF/6.cpp
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-float taxCalc(float income)
+float taxCalc(float income, float *netIncome)
 {
     float tax = 0;
-    if(income > 50000)
+
+    if (income > 50000)
         tax = (income - 50000) * 0.20 + 3000;
-    else if(income > 20000)
+    else if (income > 20000)
         tax = (income - 20000) * 0.10;
-    printf("Tax = %.2f\n", tax);
-    printf("Net Income = %.2f\n", income - tax);
+
+    *netIncome = income - tax;
     return tax;
 }
+
 int main()
 {
-    float inc;
+    float inc, tax, net;
     printf("Enter income: ");
     scanf("%f", &inc);
-    taxCalc(inc);
+
+    tax = taxCalc(inc, &net);
+
+    printf("Tax = %.2f\n", tax);
+    printf("Net Income = %.2f\n", net);
+
     return 0;
 }
-
